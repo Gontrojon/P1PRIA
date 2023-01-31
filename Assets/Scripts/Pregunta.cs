29d599fde208d794b668275097853a0e3ae6680b
Jonatan Troyano <leonidasnegro3@gmail.com>
2023-01-31 18:10:30 +0100

    Creacion de Scripts necesarios

---
+++ Assets/Scripts/Pregunta.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Clase que almacenara los datos de cada pregunta
+[System.Serializable]
+public class Pregunta
+{
+}
---
+++ Assets/Scripts/Preguntas.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// clase que almacenara la cabecera de las preguntas
+[System.Serializable]
+public class Preguntas
+{
+}

4f9a83b77e756f17b9a1038e8f96b464795975d5
Jonatan Troyano <leonidasnegro3@gmail.com>
2023-01-31 18:23:46 +0100

    Implementacion de las Clases Pregunta y Preguntas

--- Assets/Scripts/Pregunta.cs
+++ Assets/Scripts/Pregunta.cs
@@ -6,4 +6,10 @@ using UnityEngine;
 [System.Serializable]
 public class Pregunta
 {
+    public string category;
+    public string type;
+    public string difficulty;
+    public string question;
+    public string correct_answer;
+    public string[] incorrect_answers;
 }
